1c9bfecf60dd2409c09531d8e3077386d15acc79
Mohit Shahi <moheetshahi92@gmail.com>
2025-01-04 16:42:52 +0545

    computed first

--- src/utils/first.ts
+++ src/utils/first.ts
@@ -1,18 +1,64 @@
-type Grammar = [string, string][];
+import { FirstFollow, Grammar } from "../types/types";
 
-// firsts {
-//     'S': [_, _,_],
-//     'C': [_, _, _]
-// }
-type First = Record<string, string[]>;
+const epsilon = "ε";
+
 export const first = function (
   grammar: Grammar,
   terminals: string[],
   nonTerminals: string[]
-) {
-  const first: First = {};
-  terminals.forEach((terminal) => (first[terminal] = [terminal]));
-  console.log(first);
+): FirstFollow {
+  const first: FirstFollow = {};
+  terminals.forEach((symbol) => (first[symbol] = [symbol]));
+  nonTerminals.forEach((symbol) => (first[symbol] = []));
+
+  let updated = true;
+  while (updated) {
+    updated = false;
+    grammar.forEach((rule) => {
+      const symbol = rule[0];
+      const production = rule[1];
+      let newFirst: string[] = [];
+      if (production === epsilon) {
+        newFirst = [production];
+      } else if (terminals.includes(production)) {
+        newFirst = [production];
+      } else {
+        for (const token of production) {
+          if (terminals.includes(token)) {
+            newFirst = [...newFirst, token];
+            break;
+          } else if (nonTerminals.includes(token)) {
+            newFirst = [
+              ...newFirst,
+              ...first[token].filter((x) => x !== epsilon),
+            ];
+          }
+          if (!first[token].includes(epsilon)) break;
+        }
+      }
+
+      if (
+        production
+          .split("")
+          .every(
+            (token) =>
+              nonTerminals.includes(token) && first[token].includes(epsilon)
+          )
+      ) {
+        newFirst.push(epsilon);
+      }
+
+      const uniqueFirst = Array.from(new Set([...first[symbol], ...newFirst]));
+      //if first of symbole is added update
+      if (uniqueFirst.length > first[symbol].length) {
+        first[symbol] = uniqueFirst;
+        updated = true;
+      }
+    });
+  }
+
+  // console.log(first);
+  return first;
 };
 
 const testGrammar: Grammar = [
@@ -25,3 +71,4 @@ const testGrammar: Grammar = [
 ];
 const nonTerminals: string[] = ["E", "T", "F"];
 const terminals: string[] = ["+", "*", "id", "(", ")"];
+first(testGrammar, terminals, nonTerminals);
