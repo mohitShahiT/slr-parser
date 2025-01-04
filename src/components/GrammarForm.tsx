2e302c1512b113178abfd10e66923004de1f3542
sushankhya-cpgn <chapagain.sushankhya@gmail.com>
2025-01-04 12:51:53 +0545

    Space problem fixed using regex

--- src/components/GrammarForm.tsx
+++ src/components/GrammarForm.tsx
@@ -1,35 +1,39 @@
-import {  useState } from "react"
-import {useGrammar} from "../contexts/GrammarContext"
+import { useState } from "react";
+import { useGrammar } from "../contexts/GrammarContext";
 
+export const GrammarForm = function () {
+  const [inputGrammar, setInputGrammar] = useState("");
+  const { createGrammar } = useGrammar();
 
-export const GrammarForm = function(){
-    const [inputGrammar, setInputGrammar] = useState("");
-    const{createGrammar} = useGrammar()
+  const handleGrammarChange = function (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) {
+    setInputGrammar(e.target.value);
+  };
 
-    const handleGrammarChange = function(e:React.ChangeEvent<HTMLTextAreaElement>){
-      setInputGrammar(e.target.value)
-    }
+  const handleGrammarSubmit = function (e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(inputGrammar);
+    createGrammar(inputGrammar.replace(/\s+/g, ""));
+  };
 
-
-    const handleGrammarSubmit = function(e:React.FormEvent<HTMLFormElement>){
-      e.preventDefault();
-      createGrammar(inputGrammar.replace(" ", ""));
-
-    }
-
-    return <>
-    <form onSubmit={handleGrammarSubmit}>
+  return (
+    <>
+      <form onSubmit={handleGrammarSubmit}>
         <label htmlFor="grammar">Enter Grammar:</label>
         <textarea
-        className="border-solid border-2 border-sky-500"
-        id="grammar"
-        value={inputGrammar}
-        onChange={handleGrammarChange}
-        rows={5}
-        cols={30}
-        placeholder="Type the grammar here..."
-      />
-      <button className="border-solid border-2 border-sky-500" type="submit">Enter</button>
+          className="border-solid border-2 border-sky-500"
+          id="grammar"
+          value={inputGrammar}
+          onChange={handleGrammarChange}
+          rows={5}
+          cols={30}
+          placeholder="Type the grammar here..."
+        />
+        <button className="border-solid border-2 border-sky-500" type="submit">
+          Enter
+        </button>
       </form>
     </>
-}
+  );
+};
