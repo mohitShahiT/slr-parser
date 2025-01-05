cc539706d7ff7da1aee375706da06a66371c166f
Regal Adhikari <65963700+RegalAdhikari@users.noreply.github.com>
2025-01-05 11:51:41 +0545

    ui colorfuk

--- src/components/GrammarForm.tsx
+++ src/components/GrammarForm.tsx
@@ -18,22 +18,26 @@ export const GrammarForm = function () {
   };
 
   return (
-    <>
-      <form onSubmit={handleGrammarSubmit}>
+      <>
+        <div className= "">
+          <p>GRAMMAR FORM</p>
         <label htmlFor="grammar">Enter Grammar:</label>
+        <form onSubmit={handleGrammarSubmit}>
+
         <textarea
-          className="border-solid border-2 border-sky-500"
-          id="grammar"
-          value={inputGrammar}
-          onChange={handleGrammarChange}
-          rows={5}
-          cols={30}
-          placeholder="Type the grammar here..."
+            className="border-solid border-2 border-sky-500"
+            id="grammar"
+            value={inputGrammar}
+            onChange={handleGrammarChange}
+            rows={5}
+            cols={30}
+            placeholder="Type the grammar here..."
         />
-        <button className="border-solid border-2 border-sky-500" type="submit">
-          Enter
-        </button>
-      </form>
-    </>
+          <button className="border-solid border-2 border-sky-500" type="submit">
+            Enter
+          </button>
+        </form>
+        </div>
+      </>
   );
 };
